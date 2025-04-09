d4a4ce85abf03dc0763eac919f16603e6e0eebb6
Pyry Juvonen <pyryjuvonen@gmail.com>
2025-04-09 22:28:02 +0300

    add freetube conversion function

---
+++ src/freetube_convert.py
@@ -0,0 +1,20 @@
+import playlists
+import json
+
+
+
+def convert_from_freetube(new_playlist_name: str, freetube_playlist_name: str, freetube_file_path: str):
+    with open(freetube_file_path, 'r') as file:
+        lines = file.readlines()
+        jsons = map(lambda line: json.loads(line), lines)
+        ftube_playlist = list(filter(lambda j: j["playlistName"] == freetube_playlist_name, jsons))[0]
+        
+        video_ids = list(map(lambda video: video["videoId"], ftube_playlist["videos"]))
+
+        playlists.unsafe_write_playlist(video_ids, new_playlist_name)
+
+
+
+
+
+
