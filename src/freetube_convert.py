63c39bc85f7533d556ac4b02a29ce6d85392066b
Pyry Juvonen <pyryjuvonen@gmail.com>
2025-04-10 23:04:17 +0300

    add automatic metadata conversion from freetube files

--- src/freetube_convert.py
+++ src/freetube_convert.py
@@ -1,6 +1,6 @@
 import playlists
 import json
-
+import metadata
 
 
 def convert_from_freetube(new_playlist_name: str, freetube_playlist_name: str, freetube_file_path: str):
@@ -15,6 +15,17 @@ def convert_from_freetube(new_playlist_name: str, freetube_playlist_name: str, f
 
 
 
+def get_metadata_from_freetube(freetube_playlist_name: str, freetube_file_path: str):
+    with open(freetube_file_path, 'r') as file:
+        lines = file.readlines()
+        jsons = map(lambda line: json.loads(line), lines)
+        ftube_playlist = list(filter(lambda j: j["playlistName"] == freetube_playlist_name, jsons))[0]
+        
+        for entry in ftube_playlist["videos"]:
+            metadata.write_video_metadata(entry["videoId"], {
+                "title": entry["title"],
+                "channel": entry["author"],
+            })
 
 
 
